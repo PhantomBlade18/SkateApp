568d9cd155e84235d3989403c30992839cb7fc93
PhantomBlade18 <73357427+PhantomBlade18@users.noreply.github.com>
2021-03-11 11:22:20 +0000

    Updated Reverse Geocoder

--- SkateNLocate/mainapp/static/mainapp/scripts.js
+++ SkateNLocate/mainapp/static/mainapp/scripts.js
@@ -12,7 +12,7 @@ function initMap() {
     const uluru = { lat: -24.344, lng: 132.036 };
     // The map, centered at Uluru
     map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
+        zoom: 14,
         center: pos,
     });
     // The marker, positioned at Uluru
@@ -22,7 +22,7 @@ function initMap() {
     });
 
     geocoder = new google.maps.Geocoder();
-    test()
+    
 }
 
 function getPosition() {
@@ -31,14 +31,15 @@ function getPosition() {
     map.setCenter(f);
 }
 
-$('#testButton').click(getPosition);
-$('#search').click(getAddress)
+$('#search').click(findAddress)
 
-function getAddress() {
+//searches for position, zooms to it and places down a marker
+function findAddress() {
     address = $('#address').val()
-    geocoder.geocode({ address: address }, (results, status) => {
+    geocoder.geocode({ address: address, region: "uk" }, (results, status) => {
         if (status === 'OK') {
             map.setCenter(results[0].geometry.location);
+            console.log(results)
             new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location
@@ -50,6 +51,65 @@ function getAddress() {
     });
 }
 
+//function translates stored lat long coordinates into an address to help the user find park
+function geoCodeAddress(latlng) {
+    geocoder.geocode({ location: latlng}, (results, status) => {
+        if (status === 'OK') {
+            console.log(results[0])
+            return results[0].formatted_address;
+        }
+        else {
+            console.log("Geocode was not succeessful. Reason: " + status);
+        }
+    });
+}
+
+
+//homes in on users rough location based on coordinates provided
+$('#nearMe').click(function(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            map.setCenter(pos)
+            $.ajax({
+                method: 'POST',
+                url: 'recommendations/',
+                headers: { "X-CSRFToken": $("[name=csrfmiddlewaretoken]").val() },
+                data: {
+                    lat: pos.lat,
+                    lng: pos.lng,
+                },
+                success: function (data) {
+                    var obj = JSON.parse(data)
+                    console.log("boop");
+                    console.log(obj[0])
+                    for (s in obj) {
+                        console.log(obj[s].name)
+                        text = '<div id="' + obj[s].id + '" class="skatepark">'
+                        text += '<h3>' + obj[s].name + '</h3>'
+                        latlng = { lat: parseFloat(obj[s].lat), lng: parseFloat(obj[s].long) }
+                        add = geoCodeAddress(latlng)
+                        text += '<p id= "address-line">' + +'</p>'
+                        text += '</div>'
+                        $('#skateparks-list').append(text)
+
+                    }
+                }
+            })
+        })
+    }
+    else {
+        alert("Browser does not support Location Tracking");
+    }
+})
+
+
+
+//auto triggers specific button
 function test() {
     $('#search').trigger('click');
 }
+
--- SkateNLocate/mainapp/views.py
+++ SkateNLocate/mainapp/views.py
@@ -5,6 +5,8 @@ from mainapp.models import Rating,Location,Member
 from mainapp.serializers import MemberSerializer
 from sklearn.metrics import euclidean_distances
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import json
+from geopy import distance
 
 
 #Imports related to pandas and the recommendation algorithm beyond this point
@@ -26,6 +28,13 @@ def index(request):
 
     #print(get_skate_recommendations(df,me).to_string())
     
+    locs = Location.objects.all() 
+    df = read_frame(locs)
+    loc = (51.548600,-0.367310)
+    df['distance'] = df.apply(lambda row:distance.distance(loc,(row.long,row.lat)),axis = 1)
+    print(df.to_string())
+    #return HttpResponse("Hello World")
+
     return render(request,'mainapp/home.html')
 
 
@@ -34,9 +43,29 @@ def index(request):
     #Basic test to see whether Pandas Works with Django (SPOILER: IT DOES)
     """   locs = Location.objects.all() 
     df = read_frame(locs)
+    df['distance'] = distance.distance(loc,(df['long'],df['lat']))
     print(df.to_string())
     return HttpResponse("Hello World") """
 
+def getRecommendations(request):
+    if request.method == 'POST':
+        print("Howdy")
+        if 'lat' and 'lng' in request.POST:
+            print(request.POST['lat'])
+            context = {'example' : "Hello World"}
+            context = json.dumps(context)
+            
+            loc = (request.POST['lat'],request.POST['lng'])
+            locs = Location.objects.all()
+            df = read_frame(locs)
+            df['distance'] = df.apply(lambda row:distance.distance(loc,(row.long,row.lat)),axis = 1)
+            print(df.to_string())
+            context = df.to_json(orient = "records")
+            return JsonResponse(context, safe = False)
+            print("Return not performed")
+        
+            
+            
 
 def get_skate_recommendations(df: pd.DataFrame, user_features: dict) -> pd.DataFrame: #ensures a pandas dataframe is returned. we pass the original df and the target anchor(who we compare against)
     features = ['avgRating','avgSurface','avgPopularity'] #list of features that are used to compute similarity between the users average ratings  and the skate parks average

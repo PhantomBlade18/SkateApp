fb0822443f6e0a08b4ecfb3143d33a1c5093348d
PhantomBlade18 <73357427+PhantomBlade18@users.noreply.github.com>
2021-03-23 10:43:18 +0000

    Fixed Login Feature for Index page

---
+++ SkateNLocate/mainapp/Recommender.py
@@ -0,0 +1,36 @@
+from mainapp.models import Rating,Location,Member
+from django_pandas.io import read_frame
+import pandas as pd
+import numpy as np
+from sklearn.metrics import euclidean_distances
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+def get_skate_recommendations(df: pd.DataFrame, user_features: dict) -> pd.DataFrame: #ensures a pandas dataframe is returned. we pass the original df and the target anchor(who we compare against)
+    features = ['avgRating','avgSurface','avgPopularity'] #list of features that are used to compute similarity between the users average ratings  and the skate parks average
+    
+
+    #create the features in a seperate frame
+    df_features = df[features].copy()
+    #df_features = normalize_features(df_features)
+
+    df_user = pd.DataFrame([user_features])
+    df_features = pd.concat([df_user, df_features], sort=False)
+    df_features = normalize_features(df_features)
+    
+    # compute the distances
+    X = df_features.values
+    Y = df_features.values[0].reshape(1, -1)
+    distances = euclidean_distances(X, Y)
+    
+    df_sorted = df.copy()
+    
+    df_sorted['similarity_distance'] = distances[1:]
+    return df_sorted.sort_values('similarity_distance').reset_index(drop=True)
+
+
+def normalize_features(df):
+    for col in df.columns:
+        # fill any NaN's with the mean
+        df[col] = df[col].fillna(df[col].mean())
+        df[col] = StandardScaler().fit_transform(df[col].values.reshape(-1, 1))
+    return df
--- SkateNLocate/mainapp/views.py
+++ SkateNLocate/mainapp/views.py
@@ -14,6 +14,7 @@ from django_pandas.io import read_frame
 import pandas as pd
 import numpy as np
 from sklearn.metrics import euclidean_distances
+from mainapp.Recommender import get_skate_recommendations
 
 def loggedin(view):
     ''' Decorator that tests whether user is logged in '''
@@ -26,6 +27,9 @@ def loggedin(view):
         else:
             return render(request,'mainapp/login.html')
     return mod_view
+
+def signup(request):
+    return render(request,'mainapp/register.html')
 
 def registerUserView(request): 
     ''' adds new user to the database '''
@@ -78,7 +82,12 @@ def logout(request, user):
 
 # Create your views here.
 def index(request):
-    return render(request,'mainapp/home.html')
+    if 'username' in request.session:
+        user = Member.objects.get(username = request.session['username'])
+        context = {'user': user ,'loggedin': True}
+        return render(request,'mainapp/home.html',context)
+    else:
+        return render(request,'mainapp/home.html')
 
 def viewProfile(request):
     return render(request,'mainapp/home.html')
@@ -101,7 +110,7 @@ def getNearestSkateparks(request):
             loc = (request.POST['lat'],request.POST['lng'])
             locs = Location.objects.all()
             df = read_frame(locs)
-            df['distance'] = df.apply(lambda row:distance.distance(loc,(row.lat,row.long)).km,axis = 1)
+            df['distance'] = df.apply(lambda row:distance.distance(loc,(row.lat,row.long),ellipsoid='WGS-80').km,axis = 1)
             df = df[df['distance']<= 20]
             print(df.to_string())
             context = df.to_json(orient = "records")
@@ -109,42 +118,6 @@ def getNearestSkateparks(request):
             print("Return not performed")
 
 
-        
-            
-            
-
-def get_skate_recommendations(df: pd.DataFrame, user_features: dict) -> pd.DataFrame: #ensures a pandas dataframe is returned. we pass the original df and the target anchor(who we compare against)
-    features = ['avgRating','avgSurface','avgPopularity'] #list of features that are used to compute similarity between the users average ratings  and the skate parks average
-    
-
-    #create the features in a seperate frame
-    df_features = df[features].copy()
-    #df_features = normalize_features(df_features)
-
-    df_user = pd.DataFrame([user_features])
-    df_features = pd.concat([df_user, df_features], sort=False)
-    df_features = normalize_features(df_features)
-    
-    # compute the distances
-    X = df_features.values
-    Y = df_features.values[0].reshape(1, -1)
-    distances = euclidean_distances(X, Y)
-    
-    df_sorted = df.copy()
-    
-    df_sorted['similarity_distance'] = distances[1:]
-    return df_sorted.sort_values('similarity_distance').reset_index(drop=True)
-
-
-def normalize_features(df):
-    for col in df.columns:
-        # fill any NaN's with the mean
-        df[col] = df[col].fillna(df[col].mean())
-        df[col] = StandardScaler().fit_transform(df[col].values.reshape(-1, 1))
-    return df
-
-def signup(request):
-    return render(request,'mainapp/register.html')
 
 
 def spare(): #Code for algo
@@ -157,7 +130,7 @@ def spare(): #Code for algo
 
     me = MemberSerializer(mem).data
 
-    #print(get_skate_recommendations(df,me).to_string())
+    print(get_skate_recommendations(df,me).to_string())
     
     locs = Location.objects.all() 
     df = read_frame(locs)
